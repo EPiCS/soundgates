dac3c60132c285eb76528754fb7bd5895ed1ad84
caiuspb <caius.cioran@gmail.com>
2013-10-28 20:01:09 +0100

    Ordnung geschafft

--- soundgates_reconos/hw/setup.sh
+++ soundgates_reconos/hw/setup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-BASE_DESIGN="base_design_ml605"
-#BASE_DESIGN="ml605_linux_14.2"
-HWTS="musicgenerator_v1_00_a"
+BASE_DESIGN="base_design_zedboard"
+HWTS="soundgates_v1_00_a"
 EDKDIR="edk"
 
 if [ -z "$RECONOS" ]
@@ -10,10 +9,6 @@ then
 	echo "ERROR: RECONOS environment variable is not set. $RECONOS must point to the root of the ReconOS repository."
 	exit 1
 fi
-
-echo "------------------------------------------------ WARNING ------------------------------------------------"
-echo "-------------------- BUG: You have to make the i2s ports external inside of XPS -------------------------"
-echo "------------------------------------------------ WARNING ------------------------------------------------"
 
 if [ -d "$EDKDIR" ]; then
   # Control will enter here if $EDKDIR exists.
@@ -39,5 +34,4 @@ cd -
 # add ReconOS to the edk project
 cp edk/system.mhs edk/system.mhs.orig
 $RECONOS/tools/mhsaddhwts.py edk/system.mhs.orig $HWTS > edk/system.mhs
-
 
