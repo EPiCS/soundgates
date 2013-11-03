1daa8649c730b2c55b3952c077f16ca081f28ca9
Gunnar Wüllrich <gwue@mail.upb.de>
2013-11-03 16:08:42 +0100

    .

--- alsa/AlsaTesting.c
+++ alsa/AlsaTesting.c
@@ -24,25 +24,18 @@ snd_pcm_t* initDevice(int record)
 	{
 		stream = SND_PCM_STREAM_CAPTURE;
 		pcm_name = strdup("plughw:0,1"); //might be different depending on platform!
-		if ((err = snd_pcm_open(&pcm_handle, pcm_name, SND_PCM_STREAM_CAPTURE,
-				0)) < 0)
-		{
-			fprintf(stderr, "cannot open audio device %s (%s)\n", pcm_name,
-					snd_strerror(err));
-			return 0;
-		}
+
 	}
 	else
 	{
 		stream = SND_PCM_STREAM_PLAYBACK;
 		pcm_name = strdup("plughw:0,0");
-		if ((err = snd_pcm_open(&pcm_handle, pcm_name, SND_PCM_STREAM_PLAYBACK,
-				0)) < 0)
-		{
-			fprintf(stderr, "cannot open audio device %s (%s)\n", pcm_name,
-					snd_strerror(err));
-			return 0;
-		}
+	}
+	if ((err = snd_pcm_open(&pcm_handle, pcm_name, stream, 0)) < 0)
+	{
+		fprintf(stderr, "cannot open audio device %s (%s)\n", pcm_name,
+				snd_strerror(err));
+		return 0;
 	}
 
 	if ((err = snd_pcm_hw_params_malloc(&hw_params)) < 0)
@@ -93,7 +86,6 @@ snd_pcm_t* initDevice(int record)
 			&exact_rate, 0)) < 0)
 	{
 		fprintf(stderr, "cannot set sample rate (%s)\n", snd_strerror(err));
-		exit(1);
 	}
 	printf("Rate set to %i", exact_rate);
 
@@ -123,14 +115,14 @@ snd_pcm_t* initDevice(int record)
 int main(void)
 {
 	FILE* fp;
-	fp = fopen("test.wav", "r");
+	fp = fopen("./test.wav", "r");
 	int mychar;
 
 	int err;
 	int j;
 	snd_pcm_t* playback_handle = initDevice(0);
 	//snd_pcm_t* record_handle = initDevice(1);
-	char buf[512];
+	char buf[1024];
 	if ((err = snd_pcm_prepare(playback_handle)) < 0)
 	{
 		fprintf(stderr, "cannot prepare audio interface for use (%s)\n",
@@ -149,31 +141,22 @@ int main(void)
 
 		if ((nframes = snd_pcm_avail_update(playback_handle)) < 0)
 		{
-			if (nframes == -EPIPE)
-			{
-				fprintf(stderr, "Buffer underrun!\n");
-			}
-			else
-			{
-				fprintf(stderr, "unknown ALSA avail update return value (%d)\n",
-						nframes);
-			}
+			fprintf(stderr, "unknown ALSA avail update return value (%d)\n",
+					nframes);
 		}
 		else
 		{
 			printf("Alsa wants %i frames\n", nframes);
 
-
-
 			// char ist 1 Byte
 			// ein frame sind hier aber 4 Byte: 16Bit LE * 2 Channel
-			// also enthält das buffer array maximal 512/4 Frames
-			if (nframes > 512/4)
+			// also enthält das buffer array maximal 1024/4 Frames
+			if (nframes > 1024 / 4)
 			{
-				nframes = 512/4;
+				nframes = 1024 / 4;
 			}
 
-			for (j = 0; j < nframes*4; j++)
+			for (j = 0; j < nframes * 4; j += 1)
 			{
 				mychar = fgetc(fp);
 
@@ -182,11 +165,13 @@ int main(void)
 			}
 			//printf("\n");
 
-
 			if ((err = snd_pcm_writei(playback_handle, buf, nframes)) < 0)
 			{
 				fprintf(stderr, "write to audio interface failed (%s)\n",
 						snd_strerror(err));
+				// Most likely buffer underrun occured. Need to call snd_pcm_prepare again before we can send new date
+				snd_pcm_prepare(playback_handle);
+
 			}
 		}
 
