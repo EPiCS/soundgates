662e819f05c1fa90ab0f623422a982fed3a73af7
Gunnar Wüllrich <gwue@mail.upb.de>
2013-11-09 18:54:26 +0100

    Incomplete software thread stub

---
+++ software/zynq/SoundgatesZynq/src/software_sound_components/software_sound_components.h
@@ -0,0 +1,17 @@
+/*
+ * software_sound_components.h
+ *
+ *  Created on: 09.11.2013
+ *      Author: gwue
+ */
+
+#ifndef SOFTWARE_SOUND_COMPONENTS_H_
+#define SOFTWARE_SOUND_COMPONENTS_H_
+
+#include <mbox.h>
+#include <reconos.h>
+#include <pthread.h>
+
+void* create_dummy_swt_thread(void* data);
+
+#endif /* SOFTWARE_SOUND_COMPONENTS_H_ */
