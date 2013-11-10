eb01c96a71800637cefd42171e4be175efbff381
posewsky <posewsky>
2013-11-11 00:06:17 +0100

    UserInput splitted into several modules

--- software/zynq/UserInput/compile.sh
+++ software/zynq/UserInput/compile.sh
@@ -1,1 +1,5 @@
-gcc user_input.c -o user_input -Wall -lpthread -Wl,-rpath,/usr/local/lib -llo
+gcc -c -o tcp_handshake tcp_handshake.c
+gcc -c -o osc_handler osc_handler.c
+gcc -c -o user_input user_input.c
+gcc tcp_handshake osc_handler user_input -Wl,-rpath,/usr/local/lib -llo -lpthread -o ui
+rm tcp_handshake osc_handler user_input
---
+++ software/zynq/UserInput/osc_handler.c
@@ -0,0 +1,79 @@
+#include "osc_handler.h"
+
+int done = 0;
+
+int lo_main()
+{
+    /* start a new server on port 7770 */
+    lo_server_thread st = lo_server_thread_new("7770", error);
+
+    /* add method that will match any path and args */
+    lo_server_thread_add_method(st, NULL, NULL, generic_handler, NULL);
+
+    /* add method that will match the path /foo/bar, with two numbers, coerced
+     * to float and int */
+    lo_server_thread_add_method(st, "/foo/bar", "fi", foo_handler, NULL);
+
+    /* add method that will match the path /quit with no args */
+    lo_server_thread_add_method(st, "/quit", "", quit_handler, NULL);
+
+    lo_server_thread_start(st);
+
+    while (!done) {
+#ifdef WIN32
+        Sleep(1);
+#else
+        usleep(1000);
+#endif
+    }
+
+    lo_server_thread_free(st);
+
+    return 0;
+}
+
+void error(int num, const char *msg, const char *path)
+{
+    printf("liblo server error %d in path %s: %s\n", num, path, msg);
+    fflush(stdout);
+}
+
+/* catch any incoming messages and display them. returning 1 means that the
+ * message has not been fully handled and the server should try other methods */
+int generic_handler(const char *path, const char *types, lo_arg ** argv,
+                    int argc, void *data, void *user_data)
+{
+    int i;
+
+    printf("path: <%s>\n", path);
+    for (i = 0; i < argc; i++) {
+        printf("arg %d '%c' ", i, types[i]);
+        lo_arg_pp((lo_type)types[i], argv[i]);
+        printf("\n");
+    }
+    printf("\n");
+    fflush(stdout);
+
+    return 1;
+}
+
+int foo_handler(const char *path, const char *types, lo_arg ** argv,
+                int argc, void *data, void *user_data)
+{
+    /* example showing pulling the argument values out of the argv array */
+    printf("%s <- f:%f, i:%d\n\n", path, argv[0]->f, argv[1]->i);
+    fflush(stdout);
+
+    return 0;
+}
+
+int quit_handler(const char *path, const char *types, lo_arg ** argv,
+                 int argc, void *data, void *user_data)
+{
+    done = 1;
+    printf("quiting\n\n");
+    fflush(stdout);
+
+    return 0;
+}
+
