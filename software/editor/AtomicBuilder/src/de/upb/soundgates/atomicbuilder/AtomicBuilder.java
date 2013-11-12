68f877083164b457887df1d415307cbdcda2b6cc
Martin Boonk <martin@boonk.info>
2013-11-12 13:28:45 +0100

    renamed the main application file

--- software/editor/AtomicBuilder/src/de/upb/soundgates/atomicbuilder/AtomicBuilder.java
+++ software/editor/AtomicBuilder/src/de/upb/soundgates/atomicbuilder/AtomicBuilder.java
@@ -23,7 +23,7 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-public class Application {
+public class AtomicBuilder {
 
 	static boolean folder = false;
 	public static void main(String[] args) {
