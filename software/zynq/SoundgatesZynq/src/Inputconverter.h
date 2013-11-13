4904e217284bf3145348b07f128b186947d484c3
Hendrik Hangmann <h.hangmann@gmail.com>
2013-11-13 18:23:05 +0100

    frequenzumrechner an den square generator inkl duty cycle angepasst (#17)

--- software/zynq/SoundgatesZynq/src/Inputconverter.h
+++ software/zynq/SoundgatesZynq/src/Inputconverter.h
@@ -1,8 +1,16 @@
 
-// calculates the frequency of sawtooth, triangle and square wave (not sinus!) into its phase increment value
-
+// calculates the frequency of sawtooth and triangle (not square and sinus!) into its phase increment value
 float freq_to_incr (float freq)
 {
     return freq * 2.0f / 1000.0f;
 }
 
+// calculates the frequency of square into its phase increment value, depending on duty cycle ratio
+// duty_cycle = signal_ON_time / signal_OFF_time
+// duty cycle standard = 1
+float freq_to_incr (float freq, float duty_ratio)
+{
+    float duty_on  = duty_ratio;
+    float duty_off = duty_on + 1;
+    return freq * (duty_off) / 1000.0f;
+}
