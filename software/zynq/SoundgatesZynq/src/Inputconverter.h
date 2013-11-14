c532da3fcc24d2eb5acd9231eeabd7b7d4745f84
Hendrik Hangmann <h.hangmann@gmail.com>
2013-11-14 21:01:01 +0100

    Update Inputconverter.h

--- software/zynq/SoundgatesZynq/src/Inputconverter.h
+++ software/zynq/SoundgatesZynq/src/Inputconverter.h
@@ -1,4 +1,3 @@
-
 // calculates the frequency of sawtooth and triangle (not square and sinus!) into its phase increment value
 float freq_to_incr (float freq)
 {
@@ -8,7 +7,7 @@ float freq_to_incr (float freq)
 // calculates the frequency of square into its phase increment value, depending on duty cycle ratio
 // duty_cycle = signal_ON_time / signal_OFF_time
 // duty cycle standard = 1
-float freq_to_incr (float freq, float duty_ratio)
+float freq_to_incr_sq (float freq, float duty_ratio)
 {
     float duty_on  = duty_ratio;
     float duty_off = duty_on + 1;
