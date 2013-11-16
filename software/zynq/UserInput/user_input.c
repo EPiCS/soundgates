5f3f2b0466c70de03feb6f23ddc0c8396b574bc1
Hendrik Hangmann <h.hangmann@gmail.com>
2013-11-16 19:49:00 +0100

    OSC handler and user input (transfer to synthesizert!)

--- software/zynq/UserInput/user_input.c
+++ software/zynq/UserInput/user_input.c
@@ -1,20 +1,52 @@
 #include "user_input.h"
+#include "SynthesizerTest.h"
+
+float offset_sin;
+
+float offset_tri;
+
+int[2] osc_ret; // osc value
 
 int main()
 {
-    pthread_t pthread_t_tcp_handshake, pthread_t_osc_handler;
+    // TODO: offsets abfangen
+    offset_sin = 0;
+    offset_tri = 0;
+   
+// TODO: TO SYNTHESIZER:
+    osc_ret* = pthread_create(&pthread_t_osc_handler, NULL, osc_handler_thread, (void*)NULL);
+// ----
+
+    if (osc_ret[0] == SINUS) {
+        // send frequency
+        set_frequency_sin(osc_ret[1], offset_sin);
+    } else if (osc_ret[0] == TRIANGLE) {
+        // send frequency
+        set_frequency_tri(osc_ret[1], offset_tri);
+    } else if (osc_ret[0] == SINUS) {
+        // TODO 
+    }
+
+    set_frequency_sin(freq_sin, offset_sin);
+    set_frequency_tri(freq_tri, offset_tri);    
+   
+    return 0;
+
+/*  pthread_t pthread_t_tcp_handshake, pthread_t_osc_handler;
     int tcp_ret, osc_ret;
     
     tcp_ret = pthread_create(&pthread_t_tcp_handshake, NULL, tcp_handshake_thread, (void*)NULL);
     osc_ret = pthread_create(&pthread_t_osc_handler, NULL, osc_handler_thread, (void*)NULL);
     
-    /* wait for threads before return */
+    /* wait for threads before return 
     pthread_join(pthread_t_tcp_handshake, NULL);
     pthread_join(pthread_t_osc_handler, NULL);
     
     printf("TCP Handshake thread returns: %d\n",tcp_ret);
     printf("OSC Handler thread returns: %d\n",osc_ret);
     return 0;
+
+*/ 
 }
 
 
--- software/zynq/include/osc_handler.h
+++ software/zynq/include/osc_handler.h
@@ -1,23 +1,23 @@
 #ifndef OSC_HANDLER_H
 #define OSC_HANDLER_H
 
+#define PORT       50050
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 #include "lo/lo.h"
-
 #include "user_input.h"
+#include "SynthesizerTest.h"
 
 
 void error(int, const char *, const char *);
 
 int generic_handler(const char *, const char *, lo_arg **, int, void *, void *);
 
-int foo_handler(const char *, const char *, lo_arg **, int, void *, void *);
+int soundgates_handler(const char *, const char *, lo_arg ** ,  int, void *, void *);
 
 int quit_handler(const char *, const char *, lo_arg **, int, void *, void *);
-
-void* osc_handler_thread(void *);
 
 #endif
