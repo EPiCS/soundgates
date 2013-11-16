635ee755633c25d95f308ac0acc8c89e58bfe1c9
Martin Boonk <martin@boonk.info>
2013-11-16 18:28:51 +0100

    added wave player atomic component

--- software/editor/Soundgates.codegen/src/soundgates/codegen/Codegen.java
+++ software/editor/Soundgates.codegen/src/soundgates/codegen/Codegen.java
@@ -417,6 +417,8 @@ public class Codegen {
 			return atomicSoundComponent.getIntegerProperties().get(propName).toString();
 		if (atomicSoundComponent.getFloatProperties().containsKey(propName))
 			return atomicSoundComponent.getFloatProperties().get(propName).toString();	
+		if (atomicSoundComponent.getStringProperties().containsKey(propName))
+			return atomicSoundComponent.getStringProperties().get(propName).toString();	
 		
 		throw new Exception("Property name not in the list!");
 	}
