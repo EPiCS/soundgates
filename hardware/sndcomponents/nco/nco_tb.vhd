7432f3f7d2ea39512dbecd39f78d5bcfe82ccc6c
Lukas Funke <lfunke@mail.upb.de>
2013-11-22 21:34:20 +0100

    Increased maximum output of cordic

--- hardware/sndcomponents/nco/nco_tb.vhd
+++ hardware/sndcomponents/nco/nco_tb.vhd
@@ -1,35 +1,6 @@
---------------------------------------------------------------------------------
--- Company: 
--- Engineer:
---
--- Create Date:   15:12:49 09/10/2013
--- Design Name:   
--- Module Name:   C:/Users/soundgates/Desktop/pg-soundgates/hardware/components/nco/nco_tb.vhd
--- Project Name:  NumericControlledOscillator
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: nco
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
---------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
  
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
 USE ieee.numeric_std.ALL;
 use STD.textio.all;
 
@@ -85,7 +56,7 @@ ARCHITECTURE behavior OF nco_tb IS
    signal o_RAMWE_nco   : std_logic := '0';   
       
 BEGIN
-   phase_incr <= Get_Cordic_Phase_Increment(FPGA_FREQUENCY, 440);
+   phase_incr <= Get_Cordic_Phase_Increment(FPGA_FREQUENCY, 1999);
    
     -- Instantiate the Unit Under Test (UUT)
    uut: nco PORT MAP (
