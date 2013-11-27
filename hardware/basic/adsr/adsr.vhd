46fb6bab356501b1a92a64d02cddb50860bdf0aa
Hendrik Hangmann <h.hangmann@gmail.com>
2013-11-27 16:08:56 +0100

    changes to basic sound component

--- hardware/basic/adsr/adsr.vhd
+++ hardware/basic/adsr/adsr.vhd
@@ -28,9 +28,10 @@ port(
         clk         : in  std_logic;
         rst         : in  std_logic;
         ce          : in  std_logic;
+        start       : in  signed(31 downto 0);
+        stop        : in  signed(31 downto 0);
         attack      : in  signed(31 downto 0); 
         decay       : in  signed(31 downto 0);  
-        sustain     : in  signed(31 downto 0);
         release     : in  signed(31 downto 0);
         start_amp   : in  signed(31 downto 0);
         attack_amp  : in  signed(31 downto 0);
@@ -45,7 +46,6 @@ architecture Behavioral of adsr is
 
     type   adsr_states is (s_attack, s_decay, s_sustain, s_release, s_exit);
     signal state  : adsr_states;
-    signal count  : signed (31 downto 0) := to_signed(integer(real( 0.0 * 2**SOUNDGATE_FIX_PT_SCALING)), 32);
     signal i_wave : signed (31 downto 0);
 
     begin
@@ -57,34 +57,36 @@ architecture Behavioral of adsr is
         begin
             if rst = '1' then
                 i_wave <= start_amp;
+                state <= s_attack;
             else
             
             if rising_edge(clk) then
                 if ce = '1' then
-                case state is
-                    when s_attack   =>
-                        i_wave <= i_wave + attack;
-                        if i_wave >= attack_amp then
-                            state <= s_decay;
-                        end if;
-                    when s_decay    =>
-                        i_wave <= i_wave - decay;
-                        if i_wave <= sustain_amp then
-                            state <= s_sustain;
-                        end if;
-                    when s_sustain  =>
-                        count <= count + to_signed(integer(real( 0.1 * 2**SOUNDGATE_FIX_PT_SCALING)), 32);
-                        if count >= to_signed(integer(real( 15.0 * 2**SOUNDGATE_FIX_PT_SCALING)), 32) then
-                            state <= s_release;
-                        end if;
-                    when s_release  =>
-                        i_wave <= i_wave - release;
-                        if i_wave <= release_amp then
-                            state <= s_exit;
-                        end if;
-							when s_exit		=>
-								--state <= s_exit;
-                end case; 
+                    if start = '1' then
+                        case state is
+                            when s_attack   =>
+                                i_wave <= i_wave + attack;
+                                if i_wave >= attack_amp then
+                                    state <= s_decay;
+                                end if;
+                            when s_decay    =>
+                                i_wave <= i_wave - decay;
+                                if i_wave <= sustain_amp then
+                                    state <= s_sustain;
+                                end if;
+                            when s_sustain  =>
+                                if stop = '1' then
+                                    state <= s_release;
+                                end if;
+                            when s_release  =>
+                                i_wave <= i_wave - release;
+                                if i_wave <= release_amp then
+                                    state <= s_exit;
+                                end if;
+					        when s_exit		=>
+						        state <= s_attack;
+                        end case;
+                    end if; 
                 end if;                       
             end if;
 		end if;
--- software/zynq/SoundgatesZynq/src/Inputconverter.c
+++ software/zynq/SoundgatesZynq/src/Inputconverter.c
@@ -7,7 +7,6 @@
 // returns increment values for wave generators (id 1 to 4)
 int freq_to_incr (int comp_id, float freq)
 {
-
     switch (comp_id)
     {
         case ID_SIN: // Sinus
@@ -27,3 +26,20 @@ int freq_to_incr (int comp_id, float freq)
     }
     return 0;
 }
+
+// returns increment values for the adsr depending on height of attack, sustain and release phase
+int ms_to_attack (int ms, float attack_height)
+{
+    return ((attack_height * freq) / SAMPLE_RATE) * SOUNDGATES_FIXED_PT_SCALE;
+}
+
+int ms_to_sustain (int ms, float attack_height, float sustain_height)
+{
+   return (((attack_height - sustain_height) * freq) / SAMPLE_RATE) * SOUNDGATES_FIXED_PT_SCALE;
+}
+
+int ms_to_release (int ms, float sustain_height, float release_height)
+{
+    return (((sustain_height - release_height) * freq) / SAMPLE_RATE) * SOUNDGATES_FIXED_PT_SCALE;
+}
+
