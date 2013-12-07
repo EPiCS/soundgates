14d2e0a1178bebb6139530c6af78c5d6c6ab2748
Hendrik Hangmann <h.hangmann@gmail.com>
2013-12-08 00:08:29 +0100

    Basic amplifier Hardware component, #48

---
+++ hardware/basic/amplifier/amplifier.vhd
@@ -0,0 +1,44 @@
+--  ____                        _             _            
+-- / ___|  ___  _   _ _ __   __| | __ _  __ _| |_ ___  ___ 
+-- \___ \ / _ \| | | | '_ \ / _` |/ _` |/ _` | __/ _ \/ __|
+--  ___) | (_) | |_| | | | | (_| | (_| | (_| | ||  __/\__ \
+-- |____/ \___/ \__,_|_| |_|\__,_|\__, |\__,_|\__\___||___/
+--                                |___/                    
+-- ======================================================================
+--
+--   title:        VHDL module - amplifier.vhd
+--
+--   project:      PG-Soundgates
+--   author:       Hendrik Hangmann, University of Paderborn
+--
+--   description:  amplifies samples
+--
+-- ======================================================================
+    
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use IEEE.MATH_REAL.ALL;
+
+library soundgates_v1_00_a;
+use soundgates_v1_00_a.soundgates_common_pkg.all;
+
+entity amplifier is
+port(                
+        clk       : in  std_logic;
+        rst       : in  std_logic;
+        ce        : in  std_logic;
+        wave      : in  signed(31 downto 0);
+	    percentage: in  signed(31 downto 0);       
+	    amp       : out signed(31 downto 0)
+    );
+
+end amplifier;
+
+architecture Behavioral of amplifier is
+  
+	begin
+		  
+        amp <= wave * percentage;	  
+        
+end Behavioral;
