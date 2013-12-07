28a697c2a34547d2ccbb6a2be966923ff57ff06b
Hendrik Hangmann <h.hangmann@gmail.com>
2013-12-07 22:05:35 +0100

    Solution for cascaded NCOs, Closed #55

--- software/zynq/SoundgatesZynq/src/Inputconverter.c
+++ software/zynq/SoundgatesZynq/src/Inputconverter.c
@@ -29,29 +29,37 @@ int freq_to_incr (int comp_id, float freq)
 // returns increment values for the adsr depending on amplitudes of attack, sustain and release phase
 int ms_to_attack (int ms, float attack_amp)
 {
-    return ((attack_amp * (1 / (ms / 100))) / SAMPLE_RATE) * SOUNDGATES_FIXED_PT_SCALE;
+    return ((attack_amp * (100 / ms)) / SAMPLE_RATE) * SOUNDGATES_FIXED_PT_SCALE;
 }
 
 int ms_to_sustain (int ms, float attack_amp, float sustain_amp)
 {
-   return (((attack_amp - sustain_amp) * (1 / (ms / 100))) / SAMPLE_RATE) * SOUNDGATES_FIXED_PT_SCALE;
+   return (((attack_amp - sustain_amp) * (100 / ms)) / SAMPLE_RATE) * SOUNDGATES_FIXED_PT_SCALE;
 }
 
 int ms_to_release (int ms, float sustain_height, float release_amp)
 {
-    return (((sustain_amp - release_amp) * (1 / (ms / 100))) / SAMPLE_RATE) * SOUNDGATES_FIXED_PT_SCALE;
+    return (((sustain_amp - release_amp) * (100 / ms)) / SAMPLE_RATE) * SOUNDGATES_FIXED_PT_SCALE;
 }
 
 // returns increment values for ramp envelope depending on amplitudes
-
-int ms_to_incr ( int ms, float start_amp, float ramp_amp)
+int ms_to_incr ( int ms, float ramp_amp, float start_stop_amp)
 {
-    return (((ramp_amp - start_amp) * (1 / (ms / 100))) / SAMPLE_RATE) * SOUNDGATES_FIXED_PT_SCALE;
+    return (((ramp_amp - start_amp) * (100 / ms)) / SAMPLE_RATE) * SOUNDGATES_FIXED_PT_SCALE;
 }
 
-int ms_to_incr ( int ms, float ramp_amp, float stop_amp)
-{
-    return (((ramp_amp - stop_amp) * (1 / (ms / 100))) / SAMPLE_RATE) * SOUNDGATES_FIXED_PT_SCALE;
-}
 
+// frequency control for a NCO
+// calculates increment out of wave output, dependant on min and max frequency
+// 
+// function:
+//      f(wave, min, max) = (max-min)/2^28 *(wave*2) + min
+//
+// usage (control wave_1 via wave_0):
+//      wave_1_increment = wave_to_incr(wave_0_output, comp_id[wave_0], 220, 440)    
+// 
+float wave_to_incr (int wave, int comp_id, min_freq, max_freq)
+{
+    return freq_to_incr(comp_id, (max-min)/2^28 *(wave*2) + min);
+}
 
