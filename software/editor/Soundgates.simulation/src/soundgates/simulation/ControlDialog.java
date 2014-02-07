bd3c9b37db53ddcf9c2fea8815bf3d04ea5ba111
Martin Boonk <martin@boonk.info>
2014-02-07 12:40:20 +0100

    additional simulation controls

--- software/editor/Soundgates.simulation/src/soundgates/simulation/ControlDialog.java
+++ software/editor/Soundgates.simulation/src/soundgates/simulation/ControlDialog.java
@@ -11,6 +11,7 @@ import org.eclipse.jface.dialogs.TitleAreaDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Event;
@@ -18,6 +19,7 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Slider;
+import org.eclipse.swt.widgets.Text;
 
 import soundgates.AtomicSoundComponent;
 
@@ -50,18 +52,40 @@ public class ControlDialog extends TitleAreaDialog {
 	  }
 	  
 	private void createIoControl(Composite container) {
+
+
+		GridData wide = new GridData();
+		wide.grabExcessHorizontalSpace = true;
+		wide.horizontalAlignment = GridData.FILL;
+		
+		GridData narrow = new GridData();
+		narrow.minimumWidth = 50;
+		narrow.grabExcessHorizontalSpace = false;
+		narrow.horizontalAlignment = GridData.FILL;
+		
+		String [] labels = new String [] {"OSC", "", "Slider", "", "", "High Value", "", "Low Value", "Last sent"};
+		
+		for (String s : labels){
+			Label label = new Label(container, SWT.NONE);
+			label.setText(s);
+		}
+		
 		if (componentsToControl != null) {
 			for (final AtomicSoundComponent comp : componentsToControl) {
 				Label lblCompName = new Label(container, SWT.NONE);
 				lblCompName.setText(comp.getName());
 
-				GridData dataComp = new GridData();
-				dataComp.grabExcessHorizontalSpace = true;
-				dataComp.horizontalAlignment = GridData.FILL;
-
 				final Slider slider = new Slider(container, SWT.NONE);
-				int max = (int)(Math.round(comp.getFloatProperties().get("MaxValue") * 10000));
-				int min = (int)(Math.round(comp.getFloatProperties().get("MinValue") * 10000));
+				final Text sliderValue = new Text(container, SWT.SINGLE);
+				final Button pushButton = new Button(container, SWT.PUSH);
+				final Button highButton = new Button(container, SWT.PUSH);
+				final Text highText = new Text(container, SWT.SINGLE);
+				final Button lowButton = new Button(container, SWT.PUSH);
+				final Text lowText = new Text(container, SWT.SINGLE);
+				final Label lastSent = new Label(container, SWT.SINGLE);
+				
+				int max = (int)(Math.ceil(comp.getFloatProperties().get("MaxValue") * 10000));
+				int min = (int)(Math.floor(comp.getFloatProperties().get("MinValue") * 10000));
 				float increment = (float)(max - min)/200;
 				final int incr = increment <= 1 ? 1 : Math.round(increment);
 				slider.setIncrement(incr);
@@ -74,15 +98,90 @@ public class ControlDialog extends TitleAreaDialog {
 						slider.getSelection();
 						try {
 							float value = (float)slider.getSelection() /10000;
+							sliderValue.setText("" + value);
 							sender.send(new OSCMessage(comp.getName(), new Object[]{value}));
+							lastSent.setText("" + value);
 						} catch (IOException e) {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
 					}
 				});
-				slider.setLayoutData(dataComp);
+				slider.setLayoutData(wide);
+				 
+				
+				sliderValue.setLayoutData(narrow);
+				sliderValue.setEditable(false);
+				
+				lastSent.setLayoutData(narrow);
 
+				
+				pushButton.setLayoutData(narrow);
+				pushButton.setText("Push");
+				pushButton.addListener(SWT.Selection, new Listener() {
+					
+					@Override
+					public void handleEvent(Event event) {
+						try {
+							float value = Float.parseFloat(highText.getText());
+							sender.send(new OSCMessage(comp.getName(), new Object[]{value}));
+							lastSent.setText("" + value);
+							value = Float.parseFloat(lowText.getText());
+							sender.send(new OSCMessage(comp.getName(), new Object[]{value}));
+							lastSent.setText("" + value);
+						}catch (NumberFormatException e){
+							highText.setText("NaN");
+							lowText.setText("NaN");
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				});
+				
+				highButton.setLayoutData(narrow);
+				highButton.setText("High");
+				highText.setLayoutData(narrow);
+				highText.setText(""+ comp.getFloatProperties().get("MaxValue"));
+				highButton.addListener(SWT.Selection, new Listener() {
+					
+					@Override
+					public void handleEvent(Event event) {
+						try {
+							float value = Float.parseFloat(highText.getText());
+							sender.send(new OSCMessage(comp.getName(), new Object[]{value}));
+							lastSent.setText("" + value);
+						}catch (NumberFormatException e){
+							highText.setText("NaN");
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				});
+				
+				lowButton.setLayoutData(narrow);
+				lowText.setLayoutData(narrow);
+				lowButton.setText("Low");
+				lowText.setText(""+ comp.getFloatProperties().get("MinValue"));
+				lowButton.addListener(SWT.Selection, new Listener() {
+					
+					@Override
+					public void handleEvent(Event event) {
+						try {
+							float value = Float.parseFloat(lowText.getText());
+							sender.send(new OSCMessage(comp.getName(), new Object[]{value}));
+							lastSent.setText("" + value);
+						}catch (NumberFormatException e){
+							highText.setText("NaN");
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				});
+				
+				
 			}
 		}
 	}
@@ -92,7 +191,7 @@ public class ControlDialog extends TitleAreaDialog {
 	    Composite area = (Composite) super.createDialogArea(parent);
 	    Composite container = new Composite(area, SWT.NONE);
 	    container.setLayoutData(new GridData(GridData.FILL_BOTH));
-	    GridLayout layout = new GridLayout(2, true);
+	    GridLayout layout = new GridLayout(9, true);
 	    container.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 	    container.setLayout(layout);
 	    createIoControl(container);
