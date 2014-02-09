61f4a275ff5ea13fe26b137cd09c97879e1a4b3a
Gunnar Wüllrich <gwue@mail.upb.de>
2014-02-09 18:44:37 +0100

    Updated .sh scripts

--- software/tools/patchStarter.sh
+++ software/tools/patchStarter.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-PROJPATH=$1
+# no longer needed, should reside in the same folder
+#PROJPATH=$1
+PROJPATH=$(dirname $0)
 
 # extract zip file to working directory
 rm -r -f /tmp/soundgates 
 mkdir /tmp/soundgates
-cp $2 /tmp/soundgates
+cp $1 /tmp/soundgates
 cd /tmp/soundgates
 unzip *.zip
 TGF=`ls *.tgf`
 
 export LD_LIBRARY_PATH="$PROJPATH/lib"
-$PROJPATH/Synthesizer.elf --soundcomponents=$PROJPATH/components --patch-file=./$TGF --alsadevice=plughw:1,0
+$PROJPATH/Synthesizer.elf --patch-file=./$TGF --alsadevice=plughw:1,0
 
 #export LD_LIBRARY_PATH="/home/gwue/git/synthesizer/pg-soundgates/software/zynq/Libraries/x86_64/lib"
 #/home/gwue/git/synthesizer/pg-soundgates/software/zynq/Synthesizer/Release/Synthesizer.elf --soundcomponents=/home/gwue/git/synthesizer/pg-soundgates/software/zynq/SoundComponents/result/ --patch-file=./$TGF --alsadevice=plughw:1,0
--- software/zynq/Synthesizer/bundle.sh
+++ software/zynq/Synthesizer/bundle.sh
@@ -7,4 +7,4 @@ mkdir -p $DEST
 rsync -u -r -l ../Libraries/$2/lib $DEST
 rsync -u -r ../SoundComponents/result $DEST/components
 rsync -u -r Release/Synthesizer.elf $DEST
- 
+rsync -u -r Release/config $DEST 
