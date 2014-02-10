06b3eb5b543b9ab6a5a44e1392298ab8e2cc4e0a
Gunnar Wüllrich <gwue@mail.upb.de>
2014-02-10 20:51:31 +0100

    Add component doesn't overflow any longer

--- software/zynq/SoundComponents/src/addss/SoundAddComponent.cpp
+++ software/zynq/SoundComponents/src/addss/SoundAddComponent.cpp
@@ -36,7 +36,34 @@ void SoundAddComponent::process(void){
 	    /*float a = (*m_SoundIn_1_Port)[i];
 	    float b = (*m_SoundIn_2_Port)[i];*/
 
-	    sample = (*m_SoundIn_1_Port)[i] + (*m_SoundIn_2_Port)[i];
+		bool overflow = false;
+		bool sameSide = false;
+		bool positive = true;
+
+		int s1 = (*m_SoundIn_1_Port)[i];
+		int s2 = (*m_SoundIn_2_Port)[i];
+
+		if (s1 >= 0 && s2 >= 0)
+		{
+			sameSide = true;
+			positive = true;
+		}
+		else if (s1 <= 0 && s2 <= 0)
+		{
+			sameSide = true;
+			positive = false;
+		}
+
+	    sample = s1 + s2;
+
+	    // Positive overflow -> set to max
+	    if (positive && sameSide && sample < 0) {
+	    	sample = INT_MAX;
+	    }
+	    // negative overflow
+	    else if (!positive && sameSide && sample > 0) {
+	    	sample = INT_MIN;
+	    }
 
 	    /*if (sample > (1 << 30)){
 	        sample = (1 << 30);
