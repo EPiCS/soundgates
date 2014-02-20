b8de4a7db254c69bd45b5f9a8f2db1db65f191cc
Gunnar Wüllrich <gwue@mail.upb.de>
2014-02-20 15:05:49 +0100

    Removed old, no longer used C Projects

--- software/zynq/SoundgatesZynq/src/UserInput/compile.sh
+++
@@ -1,5 +0,0 @@
-gcc -I../include -c -o tcp_handshake tcp_handshake.c
-gcc -I../include -c -o osc_handler osc_handler.c
-gcc -I../include -c -o user_input user_input.c
-gcc tcp_handshake osc_handler user_input -Wl,-rpath,/usr/local/lib -llo -lpthread -o ui
-rm tcp_handshake osc_handler user_input
