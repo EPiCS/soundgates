9c9e96cbe5b9496a1dec5cb49ddcb2791859b21b
Martin Boonk <martin@boonk.info>
2014-03-02 19:01:01 +0100

    preleminary fix for issue #96

--- software/editor/de.upb.soundgates.simulation/src/soundgates/simulation/Activator.java
+++ software/editor/de.upb.soundgates.simulation/src/soundgates/simulation/Activator.java
@@ -10,7 +10,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "Soundgates.simulation"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "de.upb.soundgates.simulation"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
