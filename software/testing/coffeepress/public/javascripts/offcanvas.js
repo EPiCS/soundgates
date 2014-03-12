55cd176f6ee1ac7991a3867179e9e082c0ae6abd
caiuspb <caius.cioran@gmail.com>
2014-03-12 16:38:55 +0100

    date formatting

--- software/testing/coffeepress/public/javascripts/offcanvas.js
+++ software/testing/coffeepress/public/javascripts/offcanvas.js
@@ -423,6 +423,15 @@
     if (month < 10) {
       month = "0" + month;
     }
+    if (hours < 10) {
+      hours = "0" + hours;
+    }
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
     return datum + '/' + month + '/' + year + ' - ' + hours + ':' + minutes + ':' + seconds;
   };
 
