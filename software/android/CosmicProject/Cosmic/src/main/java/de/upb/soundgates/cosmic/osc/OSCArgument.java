3d2b1a602e6ff08c5f942c6968a62e74b3448f9c
posewsky <posewsky>
2014-03-16 19:58:52 +0100

    Refactoring for OSCType

--- software/android/CosmicProject/Cosmic/src/main/java/de/upb/soundgates/cosmic/osc/OSCArgument.java
+++ software/android/CosmicProject/Cosmic/src/main/java/de/upb/soundgates/cosmic/osc/OSCArgument.java
@@ -1,17 +1,15 @@
 package de.upb.soundgates.cosmic.osc;
 
-import de.upb.soundgates.cosmic.InteractionMethod;
-
 /**
  * Created by posewsky on 16.11.13.
  */
-public class OSCType {
+public class OSCArgument {
     protected OSCMessage msg;
     protected char typeTag;
     public float value;
     public float MIN_VALUE, MAX_VALUE;
 
-    private OSCType(OSCMessage msg, char typeTag, float minValue, float maxValue) {
+    private OSCArgument(OSCMessage msg, char typeTag, float minValue, float maxValue) {
         this.msg        = msg;
         this.typeTag    = typeTag;
         this.MIN_VALUE  = minValue;
@@ -35,26 +33,26 @@ public class OSCType {
         return (value - MIN_VALUE) * 100f / (MAX_VALUE - MIN_VALUE);
     }
 
-    public static OSCType newInstance(OSCMessage msg, char typeTag) {
+    public static OSCArgument newInstance(OSCMessage msg, char typeTag) {
         switch(typeTag)
         {
             case 'i':
-                return new OSCType(msg, 'i', 0, 1);
+                return new OSCArgument(msg, 'i', 0, 1);
             case 'f':
-                return new OSCType(msg, 'f', 0, 1);
+                return new OSCArgument(msg, 'f', 0, 1);
             default:
                 return null;
         }
     }
 
-    public static OSCType newInstance(OSCMessage msg, char typeTag, String minValue, String maxValue) {
+    public static OSCArgument newInstance(OSCMessage msg, char typeTag, String minValue, String maxValue) {
         try {
             switch(typeTag)
             {
                 case 'i':
-                    return new OSCType(msg, 'i', Integer.parseInt(minValue.trim()), Integer.parseInt(maxValue.trim()));
+                    return new OSCArgument(msg, 'i', Integer.parseInt(minValue.trim()), Integer.parseInt(maxValue.trim()));
                 case 'f':
-                    return new OSCType(msg, 'f', Float.parseFloat(minValue.trim()), Float.parseFloat(maxValue.trim()));
+                    return new OSCArgument(msg, 'f', Float.parseFloat(minValue.trim()), Float.parseFloat(maxValue.trim()));
                 default:
                     return null;
             }
