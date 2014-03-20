103fb09b97797d3cf4342ab3e068554564486d24
Hendrik Hangmann <h.hangmann@gmail.com>
2014-03-20 18:20:46 +0100

    Noise driver

---
+++ software/zynq/SoundComponents/src/noise/impl/Noise_HW.cpp
@@ -0,0 +1,61 @@
+/*
+ * TemplateSoundComponent_SW.cpp
+ *
+ *  Created on: Jan 23, 2014
+ *      Author: hendrik
+ */
+
+#include "Noise_HW.hpp"
+
+#ifndef ZYNQ
+
+/* do nothing */
+
+#else
+
+Noise_HW::Noise_HW(std::vector<std::string> params) : Noise(params), m_HWTSlot(Noise::name) {
+
+}
+
+void Noise_HW::init(){
+
+    // You can init() sound output ports to clear their buffers
+    m_SoundOut_1_Port->init();
+
+    // Control values can trigger a callback function when they change.
+    if(m_HWTSlot.isValid()){
+        /* 1. initialize mailboxes */
+        mbox_init(&m_CtrlStart, 1);
+        mbox_init(&m_CtrlStop,  1);
+
+        /* 2. initialize reconos */
+        reconos_init();
+
+        m_ReconOSResource[0].type = RECONOS_TYPE_MBOX;
+        m_ReconOSResource[0].ptr  = &m_CtrlStart;
+
+        m_ReconOSResource[1].type = RECONOS_TYPE_MBOX;
+        m_ReconOSResource[1].ptr  = &m_CtrlStop;
+
+
+        m_HWTParams.args[0] = (uint32_t) m_SoundIn_1_Port->getReadBuffer();
+        m_HWTParams.args[1] = (uint32_t) m_SoundIn_1_Port->getWriteBuffer();
+
+        reconos_hwt_setresources(&m_ReconOSThread, &m_ReconOSResource[0], 2);
+        reconos_hwt_setinitdata(&m_ReconOSThread, (void *) &m_HWTParams.args[0]);
+
+        reconos_hwt_create(&m_ReconOSThread, m_HWTSlot.getSlot(), NULL);
+
+    }
+}
+
+void Noise_HW::process(){
+
+    m_HWTParams.args[0] = (uint32_t) m_SoundIn_1_Port->getReadBuffer();
+    m_HWTParams.args[1] = (uint32_t) m_SoundOut_1_Port->getWriteBuffer();
+
+    mbox_put(&m_CtrlStart, NOISE_HWT_START);
+    mbox_get(&m_CtrlStop);
+}
+
+#endif
