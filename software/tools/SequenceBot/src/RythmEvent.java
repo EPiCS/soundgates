73f916ded7f396305147db5da06b5942c50b0eb3
Martin Boonk <martin@boonk.info>
2014-03-28 18:11:40 +0100

    added missing file

---
+++ software/tools/SequenceBot/src/RythmEvent.java
@@ -0,0 +1,19 @@
+
+public class RythmEvent extends MusicalEvent {
+
+	public RythmEvent(Chord chord, long duration, long waitingTime) {
+		super(chord, duration, waitingTime);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RythmEvent(int pitch, int modifier, long duration, long waitingTime) {
+		super(pitch, modifier, duration, waitingTime);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RythmEvent(int pitch, long duration, long waitingTime) {
+		super(pitch, duration, waitingTime);
+		// TODO Auto-generated constructor stub
+	}
+
+}
